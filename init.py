1bacc4d4dc067d2d894014d9bf351f630497e99c
vitalii <vitalii.vorobij@gmail.com>
2020-01-03 13:15:19 +0200

    First commit

--- init.py
+++ init.py
@@ -10,26 +10,3 @@ try:
     game.play(0.5)
 except GameOverException:
     print('Game over...')
-
-
-#
-# init_fruits = FruitPointCollection([Position(height // 4, width // 2)])
-# init_pos = Position(height // 2, width // 2)
-#
-# game_map = GameMap(size, init_fruits, init_pos)
-#
-# draw_game_settings = DrawGameSettings('0', 'F', '#', ' ')
-# draw_game = GameDrawer(game_map, draw_game_settings)
-#
-# game_controller = GameController()
-#
-# while True:
-#     direction = game_controller.determine()
-#
-#     game_map.move(direction)
-#
-#     time.sleep(0.25)
-#
-#     os.system('clear')
-#
-#     draw_game.draw()
